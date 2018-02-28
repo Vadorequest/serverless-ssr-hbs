123b55f4423e29726270167d3f39ad52f3114453
Dhenain Ambroise <ambroise.dhenain@gmail.com>
2018-02-28 19:07:57 +0100

    Remove dead code & comment

--- src/utils/aws.js
+++ src/utils/aws.js
@@ -9,33 +9,22 @@ import path from 'path';
 export const isHostedOnAWS = () => !!(process.env.LAMBDA_TASK_ROOT || process.env.AWS_EXECUTION_ENV);
 
 /**
- * Returns the project's root by default.
- * This is a workaround to resolve paths using an absolute path, which works both in local and AWS environments.
- * XXX Webpack messes up the __dirname which becomes unusable on AWS. (basically, __dirname value is wrong)
+ * Resolve project root directory.
+ * Works for both local and AWS environments.
  *
- * LAMBDA_TASK_ROOT should be something like "/var/task" when hosted on AWS, and "null" otherwise.
- *
- * @param additionalPath Optional argument to resolve path based on the project's root.
+ * @param additionalPath
  * @returns {string}
- * TODO Find a way to fix __dirname when using Webpack
  */
-export const isomorphicAbsolutePath = (additionalPath = '') => {
-  if(isHostedOnAWS()){
-    return path.join(process.env.LAMBDA_TASK_ROOT, additionalPath);
-  } else {
-    // XXX Doing "../.." because this file is under 2 directories levels (src/helpers)
-    return path.join(path.resolve(__dirname), '..', '..', additionalPath);
-  }
-};
-
 export const resolveProjectRoot = (additionalPath = '') => {
   return path.resolve(__dirname, '..', '..', additionalPath);
 };
 
 /**
+ * Shortcut/alias to resolve the __dirname absolute path.
+ * Works for both local and AWS environments.
  *
  * @param additionalPath Should be "__dirname"
- * @returns {*}
+ * @returns {string}
  */
 export const absoluteDirname = (additionalPath = '') => {
   return resolveProjectRoot(additionalPath);
